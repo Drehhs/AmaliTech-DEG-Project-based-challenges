fb1af74275a8cf654436a7082233fa668443dea3
abao <absbzhonore@gmail.com>
2026-04-25 15:24:03 +0200

    fix: allow all hosts for Railway deployment

--- backend/Pulse-Check/pulse_check/settings.py
+++ backend/Pulse-Check/pulse_check/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4=q3=b-=d-*y#l-k+qffde==h%o4y@3jzab(68&t--b=2)l1a4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
